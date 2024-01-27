4df050237e734faf8d916d8f3e0c99ed03c097ed
jmpleo <totrash@duck.com>
2024-01-27 16:17:33 +0300

    add smile status

--- powerline_shell/segments/root.py
+++ powerline_shell/segments/root.py
@@ -1,7 +1,19 @@
 from ..utils import BasicSegment
+from random import choice
 
 
 class Segment(BasicSegment):
+    good_emoji = [
+        ":)", ":-)", ":D", ":-D", ":]", ":-]", ":)",
+        ":-)", ":p", ":-p", ":P", ":-P", ";)", ";-)",
+        '(˵ ͡° ͜ʖ ͡°˵)', '⊂(◉‿◉)つ', 'ʕ·͡ᴥ·ʔ', 'ʕっ•ᴥ•ʔっ',
+        '( ͡° ᴥ ͡°)', 'ԅ(≖‿≖ԅ)', '(｡◕‿‿◕｡)', '(ღ˘⌣˘ღ)',
+        ]
+    bad_emoji = [
+        ":(", ":-(", ":c", ":[", ":'(", ">:(", ">:[", ">:(|",
+        '(ㆆ _ ㆆ)', 'ԅ(≖‿≖ԅ)', '☉ ‿ ⚆',
+        '(͠≖ ͜ʖ͠≖)', '(¬,‿,¬)', '( ▀ ͜͞ʖ▀)'
+        ]
     def add_to_powerline(self):
         powerline = self.powerline
         root_indicators = {
@@ -12,7 +24,9 @@ class Segment(BasicSegment):
         }
         bg = powerline.theme.CMD_PASSED_BG
         fg = powerline.theme.CMD_PASSED_FG
+        emoji = choice(self.good_emoji)
         if powerline.args.prev_error != 0:
             fg = powerline.theme.CMD_FAILED_FG
             bg = powerline.theme.CMD_FAILED_BG
-        powerline.append(root_indicators[powerline.args.shell], fg, bg, sanitize=False)
+            emoji = choice(self.bad_emoji)
+        powerline.append(f" {emoji} {root_indicators[powerline.args.shell]}", fg, bg, sanitize=False)
